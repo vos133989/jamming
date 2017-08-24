9bbdf0386c83bec6a2854bb128e1eeead4f2ebf0
Ruud Naastepad <ruudnaastepad@hotmail.com>
2017-08-24 14:32:04 +0200

    #666 comment: create arrays for Tracklists time: 1 status: In Progress

--- src/SearchResultTrackList/SearchResultTrackList.js
+++ src/SearchResultTrackList/SearchResultTrackList.js
@@ -2,15 +2,14 @@ import React, { Component } from 'react';
 import './SearchResultTrackList.css';
 import { SearchResultTrack } from '../SearchResultTrack/SearchResultTrack';
 
+const tracks = ['','' ,'' ,'' ,''];
+
 export class SearchResultTrackList extends Component {
   render() {
+    const searchResultTracks = tracks.map(track => <SearchResultTrack />);
     return (
       <div className="TrackList">
-        <SearchResultTrack />
-        <SearchResultTrack />
-        <SearchResultTrack />
-        <SearchResultTrack />
-        <SearchResultTrack />
+        {searchResultTracks}
       </div>
     );
   }
