d8be4cb425dd0f0c5bc2e6321e0ec60f9b03ebed
Ruud Naastepad <ruudnaastepad@hotmail.com>
2017-08-24 18:52:29 +0200

    #670 comment: refactor to one TrackList component time: 1 status: Done

--- src/SearchResultTracks/SearchResultTracks.js
+++ src/SearchResultTracks/SearchResultTracks.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import './SearchResultTracks.css';
-import { SearchResultTrackList } from '../SearchResultTrackList/SearchResultTrackList';
+import { TrackList } from '../TrackList/TrackList';
 
 export class SearchResultTracks extends Component {
   render() {
     return (
       <div className="SearchResults">
         <h2>Results</h2>
-        <SearchResultTrackList />
+        <TrackList action='+'/>
       </div>
     );
   }
