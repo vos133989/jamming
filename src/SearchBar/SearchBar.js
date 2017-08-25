60e83ae91ca03be4873d0cbfaef7c76d58398271
Ruud Naastepad <ruudnaastepad@hotmail.com>
2017-08-25 18:00:32 +0200

    #697 comment: term changes on update of search field time: 2 status: Done

--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -2,10 +2,21 @@ import React, { Component } from 'react';
 import './SearchBar.css';
 
 export class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { term : ''};
+    this.handleTermChange = this.handleTermChange.bind(this);
+  }
+
+  handleTermChange(e) {
+    const term = e.target.value;
+    this.setState({ term: term});
+  }
+
   render() {
     return (
       <div className="SearchBar">
-        <input placeholder="Enter A Song Title" />
+        <input onChange={this.handleTermChange} placeholder="Enter A Song Title" />
         <a>SEARCH</a>
       </div>
     );
